605e438a9a657ef7af09d300c5d00bfb3d1ded5c
zhoumin <min.zhou@pccw.com>
2020-01-08 15:54:06 +0800

    feat:one

---
+++ src/main/java/com/springboot/springbootdemo/App.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
